cc8fcb22fe36df1bb766d0b09d093cdb2d28f2d8
Vukasin Vujovic <vujovic.sv11.2023@uns.ac.rs>
2026-04-17 23:18:14 +0200

    Add config

--- snus-klk1/snus-klk1/Config.cs
+++ snus-klk1/snus-klk1/Config.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace snus_klk1
 {
     internal static class Config
     {
-        public static int allowedDelayMs = 2000;
-        public static int workerCount = 5;
-        public static int maxQueueSize = 100;
-        public static bool setupFromFile = true;
+        public static readonly int allowedDelayMs = 2000;
+        public static readonly int workerCount = 5;
+        public static readonly int maxQueueSize = 100;
+        public static readonly bool setupFromFile = true;
+        public static readonly int reportCooldownSeconds = 60;
+        public static readonly int primeRange = 1000000;
+        public static readonly int ioRangeMs = 3000;
+        public static readonly int priorityRange = 10;
     }
 }
